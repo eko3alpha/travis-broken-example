cd776839294e0e02587718975214ea44b4ce836e
aalvarado <aalvarado@angels-imac.corporate.pcdi.pri>
2016-09-09 11:48:40 -0400

    Fix broken Travis test

---
+++ tests/CalcTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class CalcTest extends PHPUnit_Framework_TestCase
+{
+	public function test_add()
+    {
+        $calc = new Calc;
+        $test = $calc->add(1)->add(1)->is();
+		$this->assertEquals($test, 2);
+  	}
+}
